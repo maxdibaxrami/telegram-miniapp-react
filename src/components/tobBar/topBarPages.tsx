31403ec11d03a49c268b90fe4c61907de11a99f9
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-11-08 19:54:54 +0300

    m

--- src/components/tobBar/topBarPages.tsx
+++ src/components/tobBar/topBarPages.tsx
@@ -1,17 +1,13 @@
 import { Navbar, NavbarContent, NavbarItem, Button } from "@nextui-org/react";
-import { useRouter } from 'next/navigation'
-
-import { BackMenu } from "../icons/topBar";
 
 const TopBarPages = () => {
-  const router = useRouter()
 
   return (
     <Navbar disableAnimation isBordered className="">
       <NavbarContent justify="start">
         <NavbarItem>
-            <Button onPress={e=> router.back()} isIconOnly aria-label="Like" color="default">
-              <BackMenu />
+            <Button onPress={()=> {}} isIconOnly aria-label="Like" color="default">
+
             </Button>
         </NavbarItem>
 
