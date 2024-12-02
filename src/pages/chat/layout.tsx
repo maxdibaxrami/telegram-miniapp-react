896af56760f7db8269922dd3443528d66ba788cd
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-12-02 20:43:29 +0300

    m

--- src/pages/chat/layout.tsx
+++ src/pages/chat/layout.tsx
@@ -1,4 +1,4 @@
-import { useViewportHeightContext } from '@/veiwPortContext';
+//import { useViewportHeightContext } from '@/veiwPortContext';
 
 
 export default function ChatLayout({
@@ -7,12 +7,11 @@ export default function ChatLayout({
   children: React.ReactNode;
 }) {
 
-  const viewportHeight = useViewportHeightContext();
+  //const viewportHeight = useViewportHeightContext();
   return (
     <section 
         className='bg-background'
         style={{
-          height: `${viewportHeight}px`,
           transition: 'height 0.3s ease',
           overflow: 'hidden',
           display: 'flex',
