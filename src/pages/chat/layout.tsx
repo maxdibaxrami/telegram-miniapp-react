6be2e644b0bac30999674e75daa653dbe09d0b91
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-12-03 20:44:57 +0300

    m

--- src/pages/chat/layout.tsx
+++ src/pages/chat/layout.tsx
@@ -1,15 +1,18 @@
+import { useViewportHeightContext } from "@/veiwPortContext";
 
 export default function ChatLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const vp = useViewportHeightContext()  
 
   return (
     <section 
       className='bg-background'
       style={{
         height: '100%',    // Sets height to 100% of parent
+        maxHeight :`${vp}px`,
         transition: 'height 0.3s ease',
         overflow: 'hidden',
         display: 'flex',
