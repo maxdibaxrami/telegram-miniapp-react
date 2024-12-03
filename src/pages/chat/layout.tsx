74f35e2aee1408f76997f655fa337f669691979a
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-12-03 18:09:19 +0300

    m

--- src/pages/chat/layout.tsx
+++ src/pages/chat/layout.tsx
@@ -1,15 +1,18 @@
+
 export default function ChatLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
 
+  
   return (
     <section 
       className='bg-background'
       style={{
-        minHeight: '100vh', // Ensures it takes full height of the parent
-        height: '100%',    // Sets height to 100% of parent
+        maxHeight:'var(--tg-viewport-stable-height)',
+        minHeight: 'var(--tg-viewport-stable-height)', // Ensures it takes full height of the parent
+        height: 'var(--tg-viewport-stable-height)',    // Sets height to 100% of parent
         transition: 'height 0.3s ease',
         overflow: 'hidden',
         display: 'flex',
