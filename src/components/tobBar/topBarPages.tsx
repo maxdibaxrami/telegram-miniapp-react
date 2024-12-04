06502bc369aa73b59f9986703e77260576f50a23
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-12-05 00:11:18 +0300

    m

--- src/components/tobBar/topBarPages.tsx
+++ src/components/tobBar/topBarPages.tsx
@@ -1,24 +1,51 @@
-import { Navbar, NavbarContent, NavbarItem, Button } from "@nextui-org/react";
-import { useNavigate } from "react-router-dom";
-import { BackMenu } from "@/Icons";
-const TopBarPages = () => {
-  const navigate = useNavigate();
+import {
+  Button,
+  Navbar,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react";
+
+import { ThemeSwitch } from "./SwitchTheme";
+
+import { useSearchParams } from "react-router-dom";
+import {RotateWords} from '@/components/animate/rotate-words'
+import { useRef } from "react";
+import NearByFilter from "../naerby/NearByFilter";
+import { FitlerIcon } from "@/Icons";
+import ExploreFilter from "../explore/exploreFilter";
+
+import { useLaunchParams } from '@telegram-apps/sdk-react';
+
+interface ExploreFilterRef {
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+
+const TopBar = () => {
+  const [searchParams] = useSearchParams();
+  const lp = useLaunchParams()
 
   return (
-    <Navbar disableAnimation className="text-default-600">
-      <NavbarContent justify="start">
-        <NavbarItem>
-            <Button size="sm" onPress={()=> {navigate("/main?page=profile")}} isIconOnly aria-label="Like" color="primary">
-              <BackMenu stroke="#FFF"/>
-            </Button>
-        </NavbarItem>
+  <>
+    <Navbar
+      className="top-0 fixed text-default-600 z-50"
+      style={{paddingTop:`${['macos', 'ios'].includes(lp.platform) ? '44px' : '0px'}` }}
+    >
 
-        <NavbarItem className="flex items-center justify-center">
-          <p className="font-bold">Glow meet</p>
+      <NavbarContent style={{gap:0}} justify="end">
+        <NavbarItem className="flex items-center">
+          <RotateWords words={"Moll Moll"} />  
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent style={{gap:0}} justify="start">
+        <NavbarItem className="flex items-center">
+          <ThemeSwitch />
         </NavbarItem>
       </NavbarContent>
     </Navbar>
+    </>
   );
 };
 
-export default TopBarPages;
+export default TopBar;
