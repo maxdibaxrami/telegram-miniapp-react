2d711f76135bd62c5ca6a9ae747cdb6d045f41ed
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-12-07 14:32:40 +0300

    m

--- src/components/explore/index.tsx
+++ src/components/explore/index.tsx
@@ -48,7 +48,7 @@ const ExplorePage = () => {
 
   return (
     <div style={{position:"relative"}} >
-        <motion.div style={{ width: "100vw", height: "calc(100vh - 25px)", position: "relative"}}>
+        <motion.div style={{ width: "100vw", height: "calc(100vh - 40px)", position: "relative"}}>
             <AnimatePresence initial={false}>
                 <ExploreCard profile={mockProfiles[index+1]} key={index + 1} frontCard={false} />
                 <ExploreCard
