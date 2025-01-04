b25b1af44ea4b06e524d80eb10f2cea113cb2a61
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-01-05 02:50:10 +0300

    '

--- src/components/wapper.tsx
+++ src/components/wapper.tsx
@@ -5,8 +5,8 @@ const MobileApp = ({children}) => {
   const viewportHeight = useViewportHeight();
 
   return (
-    <motion.div style={{overflowX:"hidden",overflowY:"auto"}} animate={{height:`${viewportHeight}px`}} className='fixed w-screen left-0 top-0 right-0 bottom-0' id="wrap">
-      <motion.div className='relative' id="content" animate={{height:`${viewportHeight}px`}}>
+    <motion.div style={{overflowX:"hidden",overflowY:"auto", backgroundColor:"red"}} animate={{height:`${viewportHeight}px`}} className='fixed w-screen left-0 top-0 right-0 bottom-0' id="wrap">
+      <motion.div style={{backgroundColor:"green"}} className='relative' id="content" animate={{height:`${viewportHeight}px`}}>
         {children}
       </motion.div>
     </motion.div>
