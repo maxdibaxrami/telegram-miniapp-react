2f5c2f67b1040f77faa225ff57434f7b1a509f3f
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-01-07 20:29:14 +0300

    m

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -10,11 +10,21 @@ import {
 
 import { useSearchParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { useLaunchParams } from "@telegram-apps/sdk-react";
 
 
 const BottomMenu = () => {
   const [searchParams] = useSearchParams();
   const { t } = useTranslation();
+  const lp = useLaunchParams();
+
+  const getPaddingForPlatform = () => {
+    if (['ios'].includes(lp.platform)) {
+      return '100px';
+    } else {
+      return '80px';
+    }
+  };
 
   return (
       <AnimatePresence >
@@ -25,39 +35,34 @@ const BottomMenu = () => {
             duration: 0.5,
           }}
           initial={{ bottom:"-120px" }}
-          animate={{ bottom: "20px" }}
+          animate={{ bottom: "0px" }}
           exit={{ bottom:"-120px"}}
   
-          className={"flex w-full fixed items-center"}
+          className={"flex w-full px-2 fixed items-center backdrop-blur	bg-background/60 backdrop-saturate-150 border-foreground/20 shadow-small"}
           style={{
             zIndex: "50",
-            width: "360px",
-            borderRadius: "14px",
+            width: "100%",
             overflow: "hidden",
             bottom:"-120px",
             justifyContent:"center",
-            height:"69px",
-            left:"50%",
-            transform:"translateX(-50%)"
-            
           }}
         >
             <Tabs
               aria-label="Options"
+              fullWidth
               classNames={{
-                tab: " h-auto p-0.5 color-white",
-                tabList: "backdrop-blur	bg-background/60 backdrop-saturate-150 border-foreground/20 border-1 shadow-small"
+                tab: " h-auto p-0.5 m-1 color-white aspect-square",
+                tabList: "bg-transparent",
               }}
               color="primary"
               size="lg"
-              radius="full"
+              radius="sm"
               selectedKey={searchParams.get("page")}
               style={{
                 zIndex: "10",
-                borderRadius: "14px",
                 overflow: "hidden",
-                height:"69px",
-                bottom:"10px",
+                height:`${getPaddingForPlatform()}`,
+                width:"100%"
 
               }}
               variant="solid" 
@@ -66,7 +71,7 @@ const BottomMenu = () => {
                 key="explore"
                 href="/#/main?page=explore"
                 title={
-                  <div style={{width:"55px", height:"55px"}} className="flex flex-col justify-center items-center">
+                  <div className="flex flex-col justify-center items-center">
                     <FireIcon/>
                     <p style={{fontSize:"11px"}}>{t('Explore')}</p>
 
@@ -78,7 +83,7 @@ const BottomMenu = () => {
                 key="nearby"
                 href="/#/main?page=nearby"
                 title={
-                  <div style={{width:"55px", height:"55px"}} className="flex flex-col justify-center items-center">
+                  <div className="flex flex-col justify-center items-center">
                     <LocationIcon/>
                     <p style={{fontSize:"11px"}}>{t('Nearby')}</p>
 
@@ -90,7 +95,7 @@ const BottomMenu = () => {
                 key="chat"
                 href="/#/main?page=chat"
                 title={
-                  <div style={{width:"55px", height:"55px"}} className="flex flex-col justify-center items-center">
+                  <div className="flex flex-col justify-center items-center">
                     <ChatIcon/>
                     <p style={{fontSize:"11px"}}>{t('Chat')}</p>
                   </div>
@@ -102,7 +107,7 @@ const BottomMenu = () => {
                 href="/#/main?page=likes"
 
                 title={
-                  <div style={{width:"55px", height:"55px"}} className="flex flex-col justify-center items-center">
+                  <div className="flex flex-col justify-center items-center">
                     <LikeIcon/>
                     <p style={{fontSize:"11px"}}>{t('Likes')}</p>
 
@@ -114,7 +119,7 @@ const BottomMenu = () => {
                 key="profile"
                 href="/#/main?page=profile"
                 title={
-                  <div style={{width:"55px", height:"55px"}} className="flex flex-col justify-center items-center">
+                  <div className="flex flex-col justify-center items-center">
                     <ProfileIcon/>
                     <p style={{fontSize:"11px"}}>{t('Profile')}</p>
                   </div>
