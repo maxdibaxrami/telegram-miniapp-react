fe87c439adaf4cfa26520a4271447a619e659e24
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-01-08 03:56:26 +0300

    m

--- src/components/naerby/nearByCard.tsx
+++ src/components/naerby/nearByCard.tsx
@@ -9,21 +9,22 @@ const NearByCard = ({ data, num }) => {
       <Card
         isFooterBlurred
         isPressable
-        radius="lg"
         className="border-none relative"
+        radius="none"
         as={Link}
         to={`/user?userId=${data.id}`}
       >
         <Image
           alt="Woman listing to music"
           className="object-cover aspect-square"
+          radius="none"
           src={`${BASEURL}${data.photo}`}
           style={{ height: "100%", width: "100%" }}
         />
-      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-            <div className=" w-full">
+        <CardFooter style={{height:"40px"}} className="justify-between px-2 border-default/20 bg-background/60 border-1 overflow-hidden py-1 absolute bottom-0 shadow-small z-10">
+          <div className=" w-full">
                 <div className="flex justify-between items-center">
-                  <p style={{textAlign:"start"}} className="flex items-center text-tiny text-white/80 text-handller">
+                  <p style={{textAlign:"start"}} className="flex items-center text-tiny text-foreground/80 text-handller">
                     {`${data.firstName} ${data.age}`}
                     {num % 2 ===0 && <CircleSvg/>} 
                     {data.verifiedAccount && <VerifyIconFill fill="#016fee" className="size-6"/> }
--- src/pages/nearby/page.tsx
+++ src/pages/nearby/page.tsx
@@ -62,12 +62,12 @@ export default function NearByPage() {
 
   return (
     <div
-      className="gap-2 grid grid-cols-2 sm:grid-cols-2 py-2"
+      className="grid grid-cols-2 sm:grid-cols-2 py-2"
       style={{
         paddingTop: "4.5rem",
         paddingBottom: "6rem",
-        paddingLeft: "12px",
-        paddingRight: "12px",
+        paddingLeft: "0px",
+        paddingRight: "0px",
       }}
     >
       {users.map((value, index) => (
